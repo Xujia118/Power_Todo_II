9537b21f7457211d00977efae58abcf2e8a1248d
Jia XU <xujia118@hotmail.com>
2024-04-28 21:22:33 -0700

    Moved user route to routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,3 +1,56 @@
 const express = require('express');
 const router = express.Router();
 const User = require("../models/User");
+
+// Import files
+const sessions = require("../sessions");
+const users = require("../users");
+
+// Get valid session
+function authenticate(req, res) {
+  const sid = req.cookies.sid;
+  const username = sid ? sessions.getSessionUser(sid) : "";
+
+  if (!sid || !users.isValid(username)) {
+    res.status(401).json({ error: "auth-missing" });
+    return "";
+  }
+
+  return username;
+}
+
+router.get("/", (req, res) => {
+  const username = authenticate(req, res);
+  if (!username) {
+    return;
+  }
+  res.status(200).json({ username });
+});
+
+router.post("/", async (req, res) => {
+  const { username } = req.body;
+
+  try {
+    // Prevent duplicate users
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(200).json({ existingUser });
+    }
+
+    // Create new user
+    if (!users.isValid(username)) {
+      return res.status(401).json({ error: "required-username" });
+    }
+
+    // Other checks here like username not too long
+
+    const sid = sessions.addSession(username);
+    const newUser = await User.create({ username });
+    res.cookie("sid", sid);
+    res.status(201).json({ newUser });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -6,14 +6,6 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 3000;
 
-// Import files
-const sessions = require("./sessions");
-const users = require("./users");
-
-
-// Temporary. Make sure it works here before moving user Router out
-const User = require('./models/User');
-
 // Connect to DB
 const connectDB = require("./connectDB");
 connectDB();
@@ -23,68 +15,15 @@ connectDB();
 app.use(express.json());
 app.use(cookieParser());
 
-// Get valid session
-function authenticate(req, res) {
-  const sid = req.cookies.sid;
-  const username = sid ? sessions.getSessionUser(sid) : "";
-
-  if (!sid || !users.isValid(username)) {
-    res.status(401).json({ error: "auth-missing" });
-    return "";
-  }
-
-  return username;
-}
-
-app.get("/api/v1/session", (req, res) => {
-  const username = authenticate(req, res);
-  if (!username) {
-    return;
-  }
-  res.status(200).json({ username });
-});
-
-
-// Temporarily leave out the checks
-// if (!users.isValid(username)) {
-//   return res.status(401).json({ error: "required-username" });
-// }
-// Other checks like "dog"
-
-app.post("/api/v1/session", async (req, res) => {
-  const { username } = req.body;
-  
-  try {
-    const sid = sessions.addSession(username);
-    const newUser = await User.create({ username });
-
-    res.cookie("sid", sid);
-    res.status(201).json({ newUser });
-
-  } catch(err) {
-    res.status(400).json({ error: err.message});
-  }
-});
-
-
-// if a user exists, get all his data
-// else create a new user
-// Of course, we need user creation logic
-// So today we need to be able to create a new user
-
-
-
-
-
-
-
-
 // Routes
-// const taskRouter = require("./routes/tasks");
-// app.use("/api/v1/tasks", taskRouter);
+const userRouter = require("./routes/users");
+app.use("/api/v1/session", userRouter);
 
-// const noteRouter = require("./routes/notes");
-// app.use("./api/v1/tasks", noteRouter);
+const taskRouter = require("./routes/tasks");
+app.use("/api/v1/tasks", taskRouter);
+
+const noteRouter = require("./routes/notes");
+app.use("/api/v1/tasks", noteRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
